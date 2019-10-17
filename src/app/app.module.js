4daaa256a7f25cb29dd446336437190f77df131e
Pradeep Kumar <pradeepkumar0509@gmail.com>
2019-10-17 09:10:15 +0530

    Main module script added

---
+++ src/app/app.module.js
@@ -0,0 +1,18 @@
+/**
+ * @file app.module.js
+ * 
+ * @description
+ * This JavaScript file contains the main module definition.
+ * 
+ * @author Pradeep Kumar
+ * @version 1.0
+ */
+(function () {
+    'use strict';
+
+    /**
+     * @description
+     * Define the module by passing an empty array as second parameter.
+     */
+    angular.module('cloudApp', []);
+});
